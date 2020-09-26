19d5e7e848188b090a6b2c7960c3b2ed5be82d9a
Kalina Kościelska <kalina.koscielska@o2.pl>
2020-09-26 11:32:20 +0200

    heading & style change & subpage creation

--- main.js
+++ main.js
@@ -1,7 +1,113 @@
-const firstName = 'Kalina'
-const age = 37
+const firstName = 'Kalina';
+const age = 37;
 
 console.log(firstName);
 console.log(age);
-alert('Hej');
-console.log(`Siema, nazywam się ${firstName} i mam ${age} lat`);
+alert("Hej");
+
+
+const heading = document.querySelector('.main-heading--js');
+
+const emptyParagraph = document.querySelector('.week-summary__description--js')
+
+emptyParagraph.innerHTML = `Nawet uzupełniłam treść javascriptem!`;
+
+function greetOld(age, firstName) {
+    console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+}
+
+const greet = (age, firstName) => {
+    console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+}
+
+function calculateOld(myNumber) {
+    return myNumber*7
+}
+
+
+const calculate = (myNumber) => myNumber*7
+
+
+
+console.log(calculate(7))
+
+
+
+greet(37, 'Kalina');
+
+
+
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+
+}
+
+createContent('.week-summary__description--js', 'Witaj świecie!')
+
+
+
+const deathStar ={
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destryed`)
+    },
+    isOperating: true,
+    levels:357,
+    name: 'Death Star',
+    population: 10000,
+    isLightOn: true,
+    comander: {
+        name: 'Darth Vader',
+        age: 44,
+    }
+}
+
+const myProperty = 'name'
+
+const showMeProperty = (myProperty) => {
+    console.log(`Twoja własność ${myProperty} to: ${deathStar[myProperty]}`);
+}
+
+showMeProperty('levels')
+
+const humanOne = {
+    name: 'Maciek',
+    age: 32
+    address: {
+        street: Warszawska
+    }
+}
+
+const humanTwo = {
+    name: 'Stefan',
+    age: humanOne.age
+}
+
+console.log(humanOne);
+console.log(humanTwo);
+
+humanOne.age = 35;
+
+
+console.log(humanOne);
+console.log(humanTwo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
